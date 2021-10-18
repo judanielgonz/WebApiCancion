408d4844dd03cc4f4586ecb52fd28e74ae34c0da
Juan daniel Gonzales Aramayo <a2020116081@estudiantes.upsa.edu.bo>
2021-10-18 01:37:15 -0400

    Cree el directorio Data y cree la clase appDbContext

---
+++ WebApiCancion/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCancion.Models;
+
+namespace WebApiCancion.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Cancion> Cancion { get; set; }
+    }
+}
